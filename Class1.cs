aba5be0a96d17dd2e2d4517dd0fda7ab466afd5e
TeyStar <teyandee.m@gmail.com>
2020-11-16 12:08:38 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace GoofballGames
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.GoofballGames.DVa_RoR2", "D.Va", "0.1.0")]
+    public class DVa_RoR2 : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            //Testing my grasp on the codebase.
+            Chat.AddMessage("Croco Smack Activated!");
+            On.EntityStates.Croco.Slash.OnEnter += (orig, self) =>
+            {
+                self.step = 2;
+                orig(self);
+            };
+        }
+    }
+}
